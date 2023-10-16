4fdb0420c2d803bfe2f9cbc18fcf792e030039c7
eklavya-joshi <eklavya.joshi@protonmail.com>
2023-10-16 15:10:32 +1100

    renamed message route functions

--- backend/src/routes/messages/messages.rs
+++ backend/src/routes/messages/messages.rs
@@ -5,7 +5,7 @@ use sqlx::PgPool;
 use uuid::Uuid;
 
 use crate::{
-    api::message::{create_dm, find_inboxes, find_messages, send_message, InboxInfo},
+    api::message::{InboxInfo, self},
     routes::error::Result, database::schema::Message, 
 };
 
@@ -52,48 +52,48 @@ pub struct GetMessages {
 
 
 #[debug_handler]
-pub async fn create(
+pub async fn create_dm(
     Extension(user): Extension<Uuid>,
     State(pool): State<PgPool>, 
     Json(payload): Json<CreateDmPayload>,
 ) -> Result<Json<CreateDmResponse>> {
     let conn = &mut pool.acquire().await?;
-    let dm = create_dm(conn, user, &payload.target).await?;
+    let dm = message::create_dm(conn, user, &payload.target).await?;
 
     Ok(Json(CreateDmResponse { dm }))
 }
 
 #[debug_handler]
-pub async fn find_inbox(
+pub async fn find_inboxes(
     Extension(user): Extension<Uuid>,
     State(pool): State<PgPool>, 
     Query(_params): Query<GetInboxes>,
 ) -> Result<Json<FindInboxResponse>> {
     let conn = &mut pool.acquire().await?;
-    let inboxes = find_inboxes(conn, user).await?;
+    let inboxes = message::find_inboxes(conn, user).await?;
 
     Ok(Json(FindInboxResponse {inboxes}))
 }
 
 #[debug_handler]
-pub async fn find_message(
+pub async fn find_messages(
     State(pool): State<PgPool>, 
     Query(params): Query<GetMessages>,
 ) -> Result<Json<FindMessagesResponse>> {
     let conn = &mut pool.acquire().await?;
-    let messages = find_messages(conn, params.inbox).await?;
+    let messages = message::find_messages(conn, params.inbox).await?;
 
     Ok(Json(FindMessagesResponse {messages}))
 }
 
 #[debug_handler]
-pub async fn send(
+pub async fn send_message(
     Extension(user): Extension<Uuid>,
     State(pool): State<PgPool>, 
     Json(payload): Json<SendMessagePayload>,
 ) -> Result<Json<SendMessageResponse>> {
     let conn = &mut pool.acquire().await?;
-    let message = send_message(conn, user, payload.inbox, &payload.content).await?;
+    let message = message::send_message(conn, user, payload.inbox, &payload.content).await?;
 
     Ok(Json(SendMessageResponse {message}))
 }
--- backend/src/routes/messages/mod.rs
+++ backend/src/routes/messages/mod.rs
@@ -1,5 +1,5 @@
 use axum::{Router, routing::{post, get}};
-use crate::routes::messages::messages::{create, find_inbox, find_message, send};
+use crate::routes::messages::messages::{create_dm, find_inboxes, find_messages, send_message};
 
 use super::AppState;
 
@@ -8,10 +8,10 @@ pub mod messages;
 pub async fn messages_router(app_state: AppState) -> Router {
 
     Router::new()
-        .route("/create/dm", post(create))
-        .route("/find/inbox", get(find_inbox))
-        .route("/find/messages", get(find_message))
-        .route("/send/message", post(send))
+        .route("/create/dm", post(create_dm))
+        .route("/find/inbox", get(find_inboxes))
+        .route("/find/messages", get(find_messages))
+        .route("/send/message", post(send_message))
         .with_state(app_state.clone())
 
 }
