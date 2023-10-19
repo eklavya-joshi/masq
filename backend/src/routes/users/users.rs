6db2e954fbaa347133bb5e1d72d50a38c85323c6
eklavya-joshi <eklavya.joshi@protonmail.com>
2023-10-19 21:57:14 +1100

    removed cookies from user login/create responses since they weren't working with the frontend

--- backend/src/routes/users/users.rs
+++ backend/src/routes/users/users.rs
@@ -34,12 +34,6 @@ pub async fn create(
 
     let token = create_user(conn, &payload.username, &payload.password).await?;
 
-    let mut cookie = Cookie::new("jwt", token.to_string());
-    cookie.set_http_only(Some(false));
-    cookie.set_secure(true);
-    cookie.set_same_site(None);
-    cookies.add(cookie);
-
     log(Json(AuthResponse { token }), "/users/create")
 }
 
@@ -53,12 +47,6 @@ pub async fn login(
     let conn = &mut pool.acquire().await?;
 
     let token = verify_user(conn, &payload.username, &payload.password).await?;
-
-    let mut cookie = Cookie::new("jwt", token.to_string());
-    cookie.set_http_only(Some(false));
-    cookie.set_secure(true);
-    cookie.set_same_site(None);
-    cookies.add(cookie);
 
     log(Json(AuthResponse { token }), "/users/login")
 
