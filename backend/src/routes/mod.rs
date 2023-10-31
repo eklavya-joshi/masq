25d2eead316048cd8fb472cde0a1d2e15392fc26
eklavya-joshi <eklavya.joshi@protonmail.com>
2023-10-31 23:32:03 +1100

    added websocket routes to router

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -21,6 +21,7 @@ use crate::middleware::auth::require_auth;
 pub use self::error::{Error, Result};
 use self::messages::messages_router;
 use self::users::{auth_users_router, noauth_users_router};
+use self::websocket::websocket_router;
 
 #[derive(Clone, FromRef)]
 pub struct AppState {
@@ -32,6 +33,7 @@ pub async fn router(app_state: Arc<AppState>) -> Router {
     Router::new()
         .nest("/users", auth_users_router(app_state.clone()).await)
         .nest("/messages", messages_router(app_state.clone()).await)
+        .nest("/ws", websocket_router(app_state.clone()).await)
         .route_layer(from_fn_with_state(app_state.clone(), require_auth))
         .nest("/users", noauth_users_router(app_state.clone()).await)
         .layer(CookieManagerLayer::new())
